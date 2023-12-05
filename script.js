4d5f14e34df08e1d5680cb5a236a284a11782665
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-06 02:03:24 +0600

    clean up code and adjustments

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const icon = document.querySelector(".js-icon");
 const body = document.querySelector("body");
 
 icon.addEventListener("click", () => {
+
   body.classList.toggle("dark-theme");
+
   if (body.classList.contains("dark-theme")) {
     icon.src = "./images/sun.png";
   } else {
