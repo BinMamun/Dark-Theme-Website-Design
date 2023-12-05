0e22af5a2baa48d6c3ef7d676f5c7296946953dc
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-06 01:54:35 +0600

    transition while changing background

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const icon = document.querySelector(".js-icon");
+const body = document.querySelector("body");
 
 icon.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
-  if (document.body.classList.contains("dark-theme")) {
+  body.classList.toggle("dark-theme");
+  if (body.classList.contains("dark-theme")) {
     icon.src = "./images/sun.png";
   } else {
     icon.src = "./images/moon.png";
